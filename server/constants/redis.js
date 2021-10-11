28ef2970e5dc3e5f180f587238ca94c5b771ed95
Mikolai Fedotov <starcrafters@ya.ru>
2021-10-11 13:20:12 +0700

    Add base wrapper from Redis

---
+++ server/constants/redis.js
@@ -0,0 +1,9 @@
+const REDIS_PRODUCTS = "products";
+const REDIS_USERS = "users";
+const REDIS_ORDERS = "orders";
+
+module.exports = {
+  REDIS_PRODUCTS,
+  REDIS_USERS,
+  REDIS_ORDERS
+};
---
+++ server/helpers/redis.js
@@ -0,0 +1,27 @@
+const IORedis = require("ioredis");
+const redis = new IORedis();
+
+class Redis {
+  name_column_redis;
+  constructor (name_column_redis) {
+    this.name_column_redis = name_column_redis
+  }
+
+  async hget(key) {
+    return await redis.hget(this.name_column_redis, key);
+  }
+
+  async hset(key, value) {
+    return await redis.hset(this.name_column_redis, key, value);
+  }
+
+  async get(key) {
+    return await redis.get(key);
+  }
+
+  async set(key, value) {
+    return await redis.set(key, value);
+  }
+}
+
+module.exports = Redis;
