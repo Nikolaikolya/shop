544b1b9cefab5290de9e643b0cc68183d749f972
Mikolai Fedotov <starcrafters@ya.ru>
2021-10-11 14:04:55 +0700

    Add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const authMeddleware = async function (req, res, next) {
+  try {
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.split(' ');
+      
+      const decoded = verifyToken(token[1]);
+
+      req.user = {
+        id: decoded.id,
+        role: decoded.role
+      };
+
+      next();
+    }
+  } catch (error) {
+
+    if(error instanceof jwt.TokenExpiredError) {
+      res.status(401).json({ success: false });
+    } else res.status(403).json({ success: false });
+
+  }
+}
+
+const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+}
+
+module.exports = authMeddleware
